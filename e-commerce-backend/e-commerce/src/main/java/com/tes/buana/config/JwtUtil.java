a1002a1d21a4d36b961c7501c6ae6ecbfa6d14f5
fian ilham pratama <fianilham1@gmail.comm>
2021-12-25 04:25:12 +0700

    forget to change jwt exp back to normal - 2h

--- e-commerce-backend/e-commerce/src/main/java/com/tes/buana/config/JwtUtil.java
+++ e-commerce-backend/e-commerce/src/main/java/com/tes/buana/config/JwtUtil.java
@@ -47,7 +47,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 /100))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
     }
 
